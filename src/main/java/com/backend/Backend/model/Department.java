78d1d2aab8032e0c6f3574dc6e9a59c3d05edf6f
Dinuki85 <prarthanadinuki2@gmail.com>
2025-06-05 21:55:03 +0530

    Added Department model class

---
+++ src/main/java/com/backend/Backend/model/Department.java
@@ -0,0 +1,66 @@
+package com.backend.Backend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.Date;
+import java.util.List;
+
+public class Department {
+    @Id
+    @Column(name="dep_id")
+    private String id;
+
+    @Column(nullable=false)
+    private String depName;
+
+    private Date established;
+
+    @JsonIgnore
+    @OneToMany(mappedBy="department")
+    private List<Employee> employees;
+
+    public Department(String id, String depName, Date established) {
+        this.id = id;
+        this.depName = depName;
+        this.established = established;
+    }
+
+    public Department() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDepName() {
+        return depName;
+    }
+
+    public void setDepName(String depName) {
+        this.depName = depName;
+    }
+
+    public Date getEstablished() {
+        return established;
+    }
+
+    public void setEstablished(Date established) {
+        this.established = established;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
