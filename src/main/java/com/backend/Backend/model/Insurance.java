732badf07db11757fbee78abd5aec9c1a791c234
Dinuki85 <prarthanadinuki2@gmail.com>
2025-06-05 21:59:26 +0530

    Created Insurance model

--- src/main/java/com/backend/Backend/model/Insurance.java
+++ src/main/java/com/backend/Backend/model/Insurance.java
@@ -1,4 +1,44 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
 public class Insurance {
+    @Id
+    @Column (name="Insurance_Id")
+    private String insId;
+    private int years;
+
+    @OneToOne
+    private Employee employee;
+
+    public Insurance() {
+        super();
+    }
+
+    public String getInsId() {
+        return insId;
+    }
+
+    public void setInsId(String insId) {
+        this.insId = insId;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
 }
